14f7a087fa778b1d0f48f15641401626fe4da1ed
xuchongjia <xuchongjia@xiaomai5.com>
2021-05-15 14:18:27 +0800

    feat:新增

---
+++ src/common/js/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+class Axios {
+  static post(method, url, params, options) {
+    const _url = `${url}`
+    return new Promise((resolve, reject) => {
+      
+    })
+  }
+}
+
+export default axios
---
+++ src/common/js/service.js
@@ -0,0 +1,8 @@
+import Axios from './axios';
+class Service {
+  static common(url, params, option) {
+    return Axios.post()
+  }
+
+
+}
